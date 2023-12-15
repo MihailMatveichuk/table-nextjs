0d756c7c57413db85071496d84f34a208e0dbb48
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-12-15 16:28:08 +0300

    feat: create newIssue api

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+import prisma from '@/prisma/client';
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+type Body = {
+  title: string;
+  description: string;
+};
+
+export async function POST(req: NextRequest) {
+  const body: Body = await req.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, {
+      status: 400,
+    });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
